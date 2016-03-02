2efbfee3d0ffc11b78b4a9c2f2cef0138242b5ba
isamukoseda <isamu.koseda@gmail.com>
2016-03-03 03:32:08 +0900

    Added files via upload

--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -11,14 +11,14 @@ exports.chargeOfSalesGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "total" : "",
+  "total" : 1,
   "Sales" : [ {
-    "firstname" : "aeiou",
-    "salesid" : "aeiou",
-    "email" : "aeiou",
-    "lastname" : "aeiou"
+    "firstname" : "山田",
+    "salesid" : "9000101",
+    "email" : "taro.yamada@netone.com",
+    "lastname" : "太郎"
   } ],
-  "id" : "aeiou"
+  "id" : "CPY9101"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -43,16 +43,16 @@ exports.companyGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "zip" : "aeiou",
-  "addresss3" : "aeiou",
-  "address2" : "aeiou",
-  "phone" : "aeiou",
-  "address1" : "aeiou",
-  "pref" : "aeiou",
-  "name" : "aeiou",
+  "zip" : "111-1111",
+  "addresss3" : "丸丸ビルディング",
+  "address2" : "丸の内",
+  "phone" : "03-1111-1111",
+  "address1" : "千代田区",
+  "pref" : "東京都",
+  "name" : "株式会社丸丸丸丸",
   "active" : true,
-  "id" : "aeiou",
-  "fax" : "aeiou"
+  "id" : "CPY9101",
+  "fax" : "03-1111-1112"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -75,12 +75,12 @@ exports.companyListGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "total" : 123,
+  "total" : 1,
   "Companies" : [ {
-    "name" : "aeiou",
-    "id" : "aeiou"
+    "name" : "株式会社丸丸丸丸",
+    "id" : "CPY9101"
   } ],
-  "email" : "aeiou"
+  "email" : "requester@netone.com"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -104,13 +104,13 @@ exports.customerGET = function(args, res, next) {
   var examples = {};
   examples['application/json'] = {
   "firstname" : "aeiou",
-  "companyid" : "aeiou",
-  "phone" : "aeiou",
-  "mobile" : "aeiou",
+  "companyid" : "CPY9101",
+  "phone" : "03-9101-0001",
+  "mobile" : "080-9101-0001",
   "active" : true,
-  "id" : "aeiou",
-  "fax" : "aeiou",
-  "email" : "aeiou",
+  "id" : "CUS9101-0001",
+  "fax" : "03-9101-0002",
+  "email" : "taro.yamada@marumaru.com",
   "lastname" : "aeiou"
 };
   
@@ -137,29 +137,29 @@ exports.mContractGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "note" : "aeiou",
-  "chargeofenginner" : "aeiou",
-  "eol" : true,
-  "contract" : "aeiou",
-  "customercontractno" : "aeiou",
-  "amountof" : "",
-  "orderby" : "aeiou",
-  "responsibleparty" : "aeiou",
-  "type" : "aeiou",
+  "note" : "契約締結に関する特記事項およびメモ：",
+  "chargeofenginner" : "9001101",
+  "eol" : false,
+  "contract" : "DISC9101-0000-00001",
+  "customercontractno" : "顧客契約番号",
+  "amountof" : 10000000,
+  "orderby" : "発注元：",
+  "responsibleparty" : "顧客担当：",
+  "type" : "契約種別：製品保守",
   "startdate" : "aeiou",
   "emergencycaller" : "aeiou",
-  "typecode" : "",
-  "caller" : "aeiou",
-  "enddate" : "aeiou",
-  "fixeddate" : "aeiou",
-  "chargeofsales" : "aeiou",
-  "contractid" : "aeiou",
-  "typeofconstraction" : "aeiou",
-  "id" : "aeiou",
+  "typecode" : 1001,
+  "caller" : "コール元会社名",
+  "enddate" : "2017.3.31",
+  "fixeddate" : "2015.4.1",
+  "chargeofsales" : "9000101",
+  "contractid" : "CONT00009101",
+  "typeofconstraction" : "構築区分：",
+  "id" : "CPY9101",
   "automaticrenewal" : true,
-  "enteringdate" : "aeiou",
-  "mastercontract" : "aeiou",
-  "status" : "aeiou"
+  "enteringdate" : "2015.4.1",
+  "mastercontract" : "BC9101-00000-000001",
+  "status" : "契約中"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -193,19 +193,19 @@ exports.mContractListGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "total" : "",
-  "index" : "",
-  "numof" : "",
-  "id" : "aeiou",
+  "total" : 1,
+  "index" : 0,
+  "numof" : 1,
+  "id" : "CPY9101",
   "Contracts" : [ {
-    "chargeofenginner" : "aeiou",
-    "enddate" : "aeiou",
-    "enduser" : "aeiou",
-    "subject" : "aeiou",
-    "contract" : "aeiou",
-    "chargeofsales" : "aeiou",
-    "customercontractno" : "aeiou",
-    "startdate" : "aeiou"
+    "chargeofenginner" : "9001101",
+    "enddate" : "2017.3.31",
+    "enduser" : "株式会社丸丸丸丸",
+    "subject" : "契約名称：機器保守《》",
+    "contract" : "DISC9101-0000-00001",
+    "chargeofsales" : "9000101",
+    "customercontractno" : "顧客契約番号",
+    "startdate" : "2015.4.1"
   } ]
 };
   
@@ -231,28 +231,28 @@ exports.productGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "eol" : true,
-  "productid" : "aeiou",
-  "dedicatedmaintenanceno" : "aeiou",
-  "contract" : "aeiou",
-  "eos" : true,
-  "sla" : "aeiou",
-  "modelno" : "aeiou",
-  "servicetypecode" : "aeiou",
-  "moniteringtype" : "aeiou",
-  "startdate" : "aeiou",
-  "servicemenu" : "aeiou",
+  "eol" : false,
+  "productid" : "製品ID",
+  "dedicatedmaintenanceno" : "専用保守部材管理番号",
+  "contract" : "個別契約番号",
+  "eos" : false,
+  "sla" : "SLA",
+  "modelno" : "メーカー型番",
+  "servicetypecode" : "サービス種別コード",
+  "moniteringtype" : "モニタリング種別",
+  "startdate" : "保守開始日",
+  "servicemenu" : "メニュー名",
   "qa" : true,
-  "servicetype" : "aeiou",
-  "enddate" : "aeiou",
-  "accountmgmtno" : "aeiou",
-  "shippingno" : "aeiou",
-  "serial" : "aeiou",
-  "productname" : "aeiou",
-  "shippingdate" : "aeiou",
-  "individualcontract" : "aeiou",
-  "servicemodelno" : "aeiou",
-  "productlistprice" : "aeiou"
+  "servicetype" : "サービス種別",
+  "enddate" : "保守終了日",
+  "accountmgmtno" : "アカウント管理番号",
+  "shippingno" : "出荷番号",
+  "serial" : "シリアル番号",
+  "productname" : "製品テキスト",
+  "shippingdate" : "出荷日時",
+  "individualcontract" : "製品／個別条件書",
+  "servicemodelno" : "サービス型番",
+  "productlistprice" : "製品定価"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -285,22 +285,22 @@ exports.productListGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "total" : "",
-  "index" : "",
-  "numof" : "",
+  "total" : 1,
+  "index" : 0,
+  "numof" : 1,
   "Product" : [ {
-    "typeofservice" : "aeiou",
-    "address" : "aeiou",
-    "enddate" : "aeiou",
-    "eol" : true,
-    "productid" : "aeiou",
-    "serial" : "aeiou",
-    "eos" : true,
-    "productname" : "aeiou",
-    "sla" : "aeiou",
-    "startdate" : "aeiou"
+    "typeofservice" : "サービス種別",
+    "address" : "住所",
+    "enddate" : "契約終了日",
+    "eol" : false,
+    "productid" : "製品ID",
+    "serial" : "シリアル番号",
+    "eos" : false,
+    "productname" : "製品テキスト",
+    "sla" : "SLA",
+    "startdate" : "契約開始日"
   } ],
-  "id" : "aeiou"
+  "id" : "CPY9101"
 };
   
   if(Object.keys(examples).length > 0) {

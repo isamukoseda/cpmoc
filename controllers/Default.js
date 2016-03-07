6a26cbc38422b58f557f6a5f6b4db23a863b4946
isamukoseda <isamu.koseda@gmail.com>
2016-03-07 18:14:18 +0900

    ver. 0.0.5

--- controllers/Default.js
+++ controllers/Default.js
@@ -6,28 +6,36 @@ var url = require('url');
 var Default = require('./DefaultService');
 
 
-module.exports.chargeOfSalesGET = function chargeOfSalesGET (req, res, next) {
-  Default.chargeOfSalesGET(req.swagger.params, res, next);
-};
-
 module.exports.companyGET = function companyGET (req, res, next) {
   Default.companyGET(req.swagger.params, res, next);
 };
 
-module.exports.companyListGET = function companyListGET (req, res, next) {
-  Default.companyListGET(req.swagger.params, res, next);
+module.exports.companyListEmailGET = function companyListEmailGET (req, res, next) {
+  Default.companyListEmailGET(req.swagger.params, res, next);
+};
+
+module.exports.companyCompanyidGET = function companyCompanyidGET (req, res, next) {
+  Default.companyCompanyidGET(req.swagger.params, res, next);
+};
+
+module.exports.contractListGET = function contractListGET (req, res, next) {
+  Default.contractListGET(req.swagger.params, res, next);
+};
+
+module.exports.contractListCompanyIdGET = function contractListCompanyIdGET (req, res, next) {
+  Default.contractListCompanyIdGET(req.swagger.params, res, next);
+};
+
+module.exports.contractContractIdGET = function contractContractIdGET (req, res, next) {
+  Default.contractContractIdGET(req.swagger.params, res, next);
 };
 
 module.exports.customerGET = function customerGET (req, res, next) {
   Default.customerGET(req.swagger.params, res, next);
 };
 
-module.exports.mContractGET = function mContractGET (req, res, next) {
-  Default.mContractGET(req.swagger.params, res, next);
-};
-
-module.exports.mContractListGET = function mContractListGET (req, res, next) {
-  Default.mContractListGET(req.swagger.params, res, next);
+module.exports.customerCustomeridGET = function customerCustomeridGET (req, res, next) {
+  Default.customerCustomeridGET(req.swagger.params, res, next);
 };
 
 module.exports.productGET = function productGET (req, res, next) {
@@ -36,4 +44,20 @@ module.exports.productGET = function productGET (req, res, next) {
 
 module.exports.productListGET = function productListGET (req, res, next) {
   Default.productListGET(req.swagger.params, res, next);
+};
+
+module.exports.productListCompanyIdGET = function productListCompanyIdGET (req, res, next) {
+  Default.productListCompanyIdGET(req.swagger.params, res, next);
+};
+
+module.exports.productProductIdGET = function productProductIdGET (req, res, next) {
+  Default.productProductIdGET(req.swagger.params, res, next);
+};
+
+module.exports.salesGET = function salesGET (req, res, next) {
+  Default.salesGET(req.swagger.params, res, next);
+};
+
+module.exports.salesCompanyidGET = function salesCompanyidGET (req, res, next) {
+  Default.salesCompanyidGET(req.swagger.params, res, next);
 };
--- controllers/DefaultService.js
+++ controllers/DefaultService.js
@@ -1,58 +1,25 @@
 'use strict';
 
-exports.chargeOfSalesGET = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-  * id (String)
-  * customerid (String)
-  * email (String)
-  **/
-  
-  
-  var examples = {};
-  examples['application/json'] = {
-  "total" : 1,
-  "Sales" : [ {
-    "firstname" : "山田",
-    "salesid" : "9000101",
-    "email" : "taro.yamada@netone.com",
-    "lastname" : "太郎"
-  } ],
-  "id" : "CPY9101"
-};
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
-}
-
 exports.companyGET = function(args, res, next) {
   /**
    * parameters expected in the args:
+  * name (String)
   * id (String)
-  * customerid (String)
-  * email (String)
   **/
   
   
   var examples = {};
   examples['application/json'] = {
-  "zip" : "111-1111",
-  "addresss3" : "丸丸ビルディング",
-  "address2" : "丸の内",
-  "phone" : "03-1111-1111",
-  "address1" : "千代田区",
-  "pref" : "東京都",
-  "name" : "株式会社丸丸丸丸",
-  "active" : true,
   "id" : "CPY9101",
-  "fax" : "03-1111-1112"
+  "name" : "株式会社丸丸丸丸",
+  "zip" : "111-1111",
+  "pref" : "東京都",
+  "address1" : "千代田区",
+  "address2" : "丸の内",
+  "addresss3" : "丸々ビルディング",
+  "phone" : "03-1111-1111",
+  "fax" : "03-1111-1112",
+  "active" : true
 };
   
   if(Object.keys(examples).length > 0) {
@@ -66,7 +33,7 @@ exports.companyGET = function(args, res, next) {
   
 }
 
-exports.companyListGET = function(args, res, next) {
+exports.companyListEmailGET = function(args, res, next) {
   /**
    * parameters expected in the args:
   * email (String)
@@ -75,12 +42,209 @@ exports.companyListGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
+  "email" : "requester@example.com",
   "total" : 1,
   "Companies" : [ {
-    "name" : "株式会社丸丸丸丸",
-    "id" : "CPY9101"
-  } ],
-  "email" : "requester@netone.com"
+  "id" : "CPY9101",
+  "name" : "株式会社丸丸丸丸",
+  "zip" : "111-1111",
+  "pref" : "東京都",
+  "address1" : "千代田区",
+  "address2" : "丸の内",
+  "addresss3" : "丸々ビルディング",
+  "phone" : "03-1111-1111",
+  "fax" : "03-1111-1112",
+  "active" : true
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.companyCompanyidGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * companyid (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "id" : "CPY9101",
+  "name" : "株式会社丸丸丸丸",
+  "zip" : "111-1111",
+  "pref" : "東京都",
+  "address1" : "千代田区",
+  "address2" : "丸の内",
+  "addresss3" : "丸々ビルディング",
+  "phone" : "03-1111-1111",
+  "fax" : "03-1111-1112",
+  "active" : true
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.contractListGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * serial (String)
+  * customerContractNo (String)
+  * enduser (String)
+  * startdate (String)
+  * enddate (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "total" : 1,
+  "index" : 1,
+  "numof" : 1,
+  "id" : "CPY9101",
+  "Contracts" : [ {
+    "note" : "契約締結に関する特記事項およびメモ：",
+    "chargeofenginner" : "9001101",
+    "eol" : flase,
+    "subject" : "サブジェクト：",
+    "amountof" : 10000000,
+    "type" : "契約種別：製品保守",
+    "startdate" : "2015.4.1",
+    "emergencycaller" : "緊急連絡先：",
+    "typecode" : 1001,
+    "companyid" : "会社ID",
+    "chargeofsales" : "9000101",
+    "automaticrenewal" : true,
+    "enteringdate" : "2015.4.1",
+    "mastercontract" : "BC9101-00000-000001",
+    "enduser" : "エンドユーザー情報：",
+    "contract" : "DISC9101-0000-00001",
+    "customercontractno" : "顧客契約番号：",
+    "orderby" : "発注元：",
+    "responsibleparty" : "顧客担当：",
+    "caller" : "コール元会社名",
+    "enddate" : "2017.3.31",
+    "fixeddate" : "2015.3.1",
+    "contractId" : "CONT00009101",
+    "typeofconstraction" : "構築区分：",
+    "status" : "契約中"
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.contractListCompanyIdGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * companyId (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "total" : 1,
+  "index" : 1,
+  "numof" : 1,
+  "id" : "CPY9101",
+  "Contracts" : [ {
+    "note" : "契約締結に関する特記事項およびメモ：",
+    "chargeofenginner" : "9001101",
+    "eol" : flase,
+    "subject" : "サブジェクト：",
+    "amountof" : 10000000,
+    "type" : "契約種別：製品保守",
+    "startdate" : "2015.4.1",
+    "emergencycaller" : "緊急連絡先：",
+    "typecode" : 1001,
+    "companyid" : "会社ID",
+    "chargeofsales" : "9000101",
+    "automaticrenewal" : true,
+    "enteringdate" : "2015.4.1",
+    "mastercontract" : "BC9101-00000-000001",
+    "enduser" : "エンドユーザー情報：",
+    "contract" : "DISC9101-0000-00001",
+    "customercontractno" : "顧客契約番号：",
+    "orderby" : "発注元：",
+    "responsibleparty" : "顧客担当：",
+    "caller" : "コール元会社名",
+    "enddate" : "2017.3.31",
+    "fixeddate" : "2015.3.1",
+    "contractId" : "CONT00009101",
+    "typeofconstraction" : "構築区分：",
+    "status" : "契約中"
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.contractContractIdGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * contractId (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+    "note" : "契約締結に関する特記事項およびメモ：",
+    "chargeofenginner" : "9001101",
+    "eol" : flase,
+    "subject" : "サブジェクト：",
+    "amountof" : 10000000,
+    "type" : "契約種別：製品保守",
+    "startdate" : "2015.4.1",
+    "emergencycaller" : "緊急連絡先：",
+    "typecode" : 1001,
+    "companyid" : "会社ID",
+    "chargeofsales" : "9000101",
+    "automaticrenewal" : true,
+    "enteringdate" : "2015.4.1",
+    "mastercontract" : "BC9101-00000-000001",
+    "enduser" : "エンドユーザー情報：",
+    "contract" : "DISC9101-0000-00001",
+    "customercontractno" : "顧客契約番号：",
+    "orderby" : "発注元：",
+    "responsibleparty" : "顧客担当：",
+    "caller" : "コール元会社名",
+    "enddate" : "2017.3.31",
+    "fixeddate" : "2015.3.1",
+    "contractId" : "CONT00009101",
+    "typeofconstraction" : "構築区分：",
+    "status" : "契約中"
 };
   
   if(Object.keys(examples).length > 0) {
@@ -103,110 +267,47 @@ exports.customerGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "firstname" : "aeiou",
-  "companyid" : "CPY9101",
+  "id" : "CUS9101-0001",
+  "firstname" : "太郎",
+  "lastname" : "山田",
+  "email" : "taro.yamada@marumaru.com",
   "phone" : "03-9101-0001",
   "mobile" : "080-9101-0001",
-  "active" : true,
-  "id" : "CUS9101-0001",
   "fax" : "03-9101-0002",
+  "companyid" : "CPY9101",
+  "active" : true
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.customerCustomeridGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * apiKey (Long)
+  * customerid (Long)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "id" : "CUS9101-0001",
+  "firstname" : "太郎",
+  "lastname" : "山田",
   "email" : "taro.yamada@marumaru.com",
-  "lastname" : "aeiou"
-};
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
-}
-
-exports.mContractGET = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-  * id (String)
-  * contract (String)
-  * customerid (String)
-  * email (String)
-  **/
-  
-  
-  var examples = {};
-  examples['application/json'] = {
-  "note" : "契約締結に関する特記事項およびメモ：",
-  "chargeofenginner" : "9001101",
-  "eol" : false,
-  "contract" : "DISC9101-0000-00001",
-  "customercontractno" : "顧客契約番号",
-  "amountof" : 10000000,
-  "orderby" : "発注元：",
-  "responsibleparty" : "顧客担当：",
-  "type" : "契約種別：製品保守",
-  "startdate" : "aeiou",
-  "emergencycaller" : "aeiou",
-  "typecode" : 1001,
-  "caller" : "コール元会社名",
-  "enddate" : "2017.3.31",
-  "fixeddate" : "2015.4.1",
-  "chargeofsales" : "9000101",
-  "contractid" : "CONT00009101",
-  "typeofconstraction" : "構築区分：",
-  "id" : "CPY9101",
-  "automaticrenewal" : true,
-  "enteringdate" : "2015.4.1",
-  "mastercontract" : "BC9101-00000-000001",
-  "status" : "契約中"
-};
-  
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
-  
-  
-}
-
-exports.mContractListGET = function(args, res, next) {
-  /**
-   * parameters expected in the args:
-  * id (String)
-  * customerid (String)
-  * email (String)
-  * contract (String)
-  * serial (String)
-  * contractor (String)
-  * customerContractNo (String)
-  * enduser (String)
-  * startdate (String)
-  * enddate (String)
-  * index (Integer)
-  * max (Integer)
-  **/
-  
-  
-  var examples = {};
-  examples['application/json'] = {
-  "total" : 1,
-  "index" : 0,
-  "numof" : 1,
-  "id" : "CPY9101",
-  "Contracts" : [ {
-    "chargeofenginner" : "9001101",
-    "enddate" : "2017.3.31",
-    "enduser" : "株式会社丸丸丸丸",
-    "subject" : "契約名称：機器保守《》",
-    "contract" : "DISC9101-0000-00001",
-    "chargeofsales" : "9000101",
-    "customercontractno" : "顧客契約番号",
-    "startdate" : "2015.4.1"
-  } ]
+  "phone" : "03-9101-0001",
+  "mobile" : "080-9101-0001",
+  "fax" : "03-9101-0002",
+  "companyid" : "CPY9101",
+  "active" : true
 };
   
   if(Object.keys(examples).length > 0) {
@@ -231,27 +332,29 @@ exports.productGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "eol" : false,
   "productid" : "製品ID",
-  "dedicatedmaintenanceno" : "専用保守部材管理番号",
+  "productName" : "製品テキスト",
+  "modelNo" : "メーカー型番",
+  "serial" : "シリアル番号",
   "contract" : "個別契約番号",
-  "eos" : false,
+  "servicemenu" : "メニュー名",
+  "typeofservice" : "サービス種別",
+  "servicetype" : "サービス種別",
   "sla" : "SLA",
-  "modelno" : "メーカー型番",
+  "servicemodelno" : "サービス型番",
   "servicetypecode" : "サービス種別コード",
   "moniteringtype" : "モニタリング種別",
-  "startdate" : "保守開始日",
-  "servicemenu" : "メニュー名",
-  "qa" : true,
-  "servicetype" : "サービス種別",
-  "enddate" : "保守終了日",
   "accountmgmtno" : "アカウント管理番号",
-  "shippingno" : "出荷番号",
-  "serial" : "シリアル番号",
-  "productname" : "製品テキスト",
-  "shippingdate" : "出荷日時",
+  "dedicatedmaintenanceno" : "専用保守部材管理番号",
   "individualcontract" : "製品／個別条件書",
-  "servicemodelno" : "サービス型番",
+  "address" : "設置場所：東京都",
+  "shippingno" : "出荷番号",
+  "shippingdate" : "出荷日時",
+  "startDate" : "2015.4.1",
+  "endDate" : "保守終了日",
+  "eos" : true,
+  "eol" : true,
+  "qa" : true,
   "productlistprice" : "製品定価"
 };
   
@@ -269,10 +372,7 @@ exports.productGET = function(args, res, next) {
 exports.productListGET = function(args, res, next) {
   /**
    * parameters expected in the args:
-  * id (String)
-  * customerid (String)
   * email (String)
-  * contract (String)
   * serial (String)
   * startdate (String)
   * enddate (String)
@@ -285,22 +385,192 @@ exports.productListGET = function(args, res, next) {
   
   var examples = {};
   examples['application/json'] = {
-  "total" : 1,
-  "index" : 0,
+  "index" : 1,
   "numof" : 1,
-  "Product" : [ {
-    "typeofservice" : "サービス種別",
-    "address" : "住所",
-    "enddate" : "契約終了日",
-    "eol" : false,
-    "productid" : "製品ID",
-    "serial" : "シリアル番号",
-    "eos" : false,
-    "productname" : "製品テキスト",
-    "sla" : "SLA",
-    "startdate" : "契約開始日"
-  } ],
-  "id" : "CPY9101"
+  "total" : 1,
+  "Products" : [ {
+  "productid" : "製品ID",
+  "productName" : "製品テキスト",
+  "modelNo" : "メーカー型番",
+  "serial" : "シリアル番号",
+  "contract" : "個別契約番号",
+  "servicemenu" : "メニュー名",
+  "typeofservice" : "サービス種別",
+  "servicetype" : "サービス種別",
+  "sla" : "SLA",
+  "servicemodelno" : "サービス型番",
+  "servicetypecode" : "サービス種別コード",
+  "moniteringtype" : "モニタリング種別",
+  "accountmgmtno" : "アカウント管理番号",
+  "dedicatedmaintenanceno" : "専用保守部材管理番号",
+  "individualcontract" : "製品／個別条件書",
+  "address" : "設置場所：東京都",
+  "shippingno" : "出荷番号",
+  "shippingdate" : "出荷日時",
+  "startDate" : "2015.4.1",
+  "endDate" : "保守終了日",
+  "eos" : true,
+  "eol" : true,
+  "qa" : true,
+  "productlistprice" : "製品定価"
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.productListCompanyIdGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * companyId (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "index" : 1,
+  "numof" : 1,
+  "total" : 1,
+  "Products" : [ {
+  "productid" : "製品ID",
+  "productName" : "製品テキスト",
+  "modelNo" : "メーカー型番",
+  "serial" : "シリアル番号",
+  "contract" : "個別契約番号",
+  "servicemenu" : "メニュー名",
+  "typeofservice" : "サービス種別",
+  "servicetype" : "サービス種別",
+  "sla" : "SLA",
+  "servicemodelno" : "サービス型番",
+  "servicetypecode" : "サービス種別コード",
+  "moniteringtype" : "モニタリング種別",
+  "accountmgmtno" : "アカウント管理番号",
+  "dedicatedmaintenanceno" : "専用保守部材管理番号",
+  "individualcontract" : "製品／個別条件書",
+  "address" : "設置場所：東京都",
+  "shippingno" : "出荷番号",
+  "shippingdate" : "出荷日時",
+  "startDate" : "2015.4.1",
+  "endDate" : "保守終了日",
+  "eos" : true,
+  "eol" : true,
+  "qa" : true,
+  "productlistprice" : "製品定価"
+  } ]
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.productProductIdGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * productId (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "productid" : "製品ID",
+  "productName" : "製品テキスト",
+  "modelNo" : "メーカー型番",
+  "serial" : "シリアル番号",
+  "contract" : "個別契約番号",
+  "servicemenu" : "メニュー名",
+  "typeofservice" : "サービス種別",
+  "servicetype" : "サービス種別",
+  "sla" : "SLA",
+  "servicemodelno" : "サービス型番",
+  "servicetypecode" : "サービス種別コード",
+  "moniteringtype" : "モニタリング種別",
+  "accountmgmtno" : "アカウント管理番号",
+  "dedicatedmaintenanceno" : "専用保守部材管理番号",
+  "individualcontract" : "製品／個別条件書",
+  "address" : "設置場所：東京都",
+  "shippingno" : "出荷番号",
+  "shippingdate" : "出荷日時",
+  "startDate" : "2015.4.1",
+  "endDate" : "保守終了日",
+  "eos" : true,
+  "eol" : true,
+  "qa" : true,
+  "productlistprice" : "製品定価"
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.salesGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * email (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "total" : 1,
+  "id" : "CPY9101",
+  "Sales" : {
+    "salesid" : "9000101",
+    "email" : "taro.yamada@example.com",
+    "firstname" : "太郎",
+    "lastname" : "山田"
+  }
+};
+  
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+  
+}
+
+exports.salesCompanyidGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * companyid (String)
+  **/
+  
+  
+  var examples = {};
+  examples['application/json'] = {
+  "total" : 1,
+  "id" : "CPY9101",
+  "Sales" : {
+    "salesid" : "9000101",
+    "email" : "taro.yamada@example.com",
+    "firstname" : "太郎",
+    "lastname" : "山田"
+  }
 };
   
   if(Object.keys(examples).length > 0) {

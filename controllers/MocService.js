abb1c95f4c44d0bcfbc049ab1f7f04767e827cb5
isamukoseda <isamu.koseda@gmail.com>
2016-03-25 15:41:42 +0900

    change data
    add tuchiya-san's data

--- controllers/MocService.js
+++ controllers/MocService.js
@@ -2,10 +2,10 @@
 
 var customer_data = {
   "customerId" : "CPY9101",
-  "firstname" : "太郎",
-  "lastname" : "山田",
+  "firstname" : "土屋",
+  "lastname" : "強",
   "companyId" : "CUS9101-0001",
-  "email" : "taro.yamada@marumaru.com",
+  "email" : "t-tuchiya@xseed.co.jp",
   "phone" : "03-9101-0001",
   "mobile" : "080-9101-0001",
   "fax" : "03-9101-0002",
@@ -23,26 +23,26 @@ var customers_data = {
 
 var company_data901 = {
   "companyId" : "CPY9101",
-  "companyName" : "株式会社９１０１",
-  "zip" : "111-1111",
+  "companyName" : "株式会社エクシード",
+  "zip" : "141-0022",
   "pref" : "東京都",
-  "address1" : "千代田区",
-  "address2" : "丸の内",
-  "address3" : "丸々ビルディング",
-  "phone" : "03-1111-1111",
-  "fax" : "03-1111-1112",
+  "address1" : "品川区",
+  "address2" : "東五反田3-20-14",
+  "address3" : "高輪パークタワー7F",
+  "phone" : "03-6422-0021",
+  "fax" : "03-6422-0022",
   "active" : true
 };
 var company_data902 = {
   "companyId" : "CPY9102",
-  "companyName" : "株式会社９１０２",
-  "zip" : "111-1111",
+  "companyName" : "ネットワンパートナーズ株式会社",
+  "zip" : "100-7026",
   "pref" : "東京都",
   "address1" : "千代田区",
-  "address2" : "丸の内",
-  "address3" : "丸々ビルディング",
-  "phone" : "03-1111-1111",
-  "fax" : "03-1111-1112",
+  "address2" : "丸の内2-7-2",
+  "address3" : "JPタワー",
+  "phone" : "03-6256-0700",
+  "fax" : "03-6256-0708",
   "active" : true
 };
 var company_data903 = {
@@ -63,7 +63,7 @@ var companies_data1 = {
   "rowNumber" : 1,
   "currentPage" : 1,
   "pageLimit" : 20,
-  "email" : "taro@example.com",
+  "email" : "taro-demo1@netone.co.jp",
   "Companies" : [company_data901]
 };
 
@@ -72,7 +72,7 @@ var companies_data2 = {
   "rowNumber" : 1,
   "currentPage" : 1,
   "pageLimit" : 20,
-  "email" : "jiro@example.com",
+  "email" : "jiro-demo2@netone.co.jp",
   "Companies" : [company_data902, company_data903]
 };
 
@@ -103,13 +103,13 @@ var saleses_data = {
 
 
 var product_data = {
-  "productId" : "製品ID",
-  "productName" : "製品テキスト",
-  "modelNo" : "メーカー型番",
+  "productId" : "NI-94J",
+  "productName" : "クラウドSD-WANエッジ端末：買取りプラン",
+  "modelNo" : "メーカー型番：",
   "serial" : "シリアル番号",
-  "contract" : "個別契約番号",
-  "individualContract" : "製品／個別条件書",
-  "dedicatedMaintenanceNo" : "専用保守部材管理番号",
+  "contract" : "DISC9101-0000-00001",
+  "individualContract" : "製品／個別条件書：なし",
+  "dedicatedMaintenanceNo" : "専用保守部材管理番号：なし",
   "typeOfService" : "サービス種別",
   "serviceTypeCode" : "サービス種別コード",
   "serviceType" : "サービス種別",
@@ -118,15 +118,15 @@ var product_data = {
   "sla" : "SLA",
   "moniteringType" : "モニタリング種別",
   "accountMgmtNo" : "アカウント管理番号",
-  "address" : "設置場所：東京都",
+  "address" : "設置場所：東京都品川区東五反田3-20-14",
   "shippingNo" : "出荷番号",
   "shippingDate" : "出荷日時",
-  "startDate" : "2015/04/01",
-  "endDate" : "保守終了日",
-  "eos" : true,
-  "eol" : true,
+  "startDate" : "2016/03/28",
+  "endDate" : "2017/03/31",
+  "eos" : false,
+  "eol" : false,
   "qa" : true,
-  "productListPrice" : "製品定価"
+  "productListPrice" : "¥600,000"
 };
 
 var products_data = {
@@ -142,28 +142,28 @@ var contract_data = {
     "note" : "契約締結に関する特記事項およびメモ：",
     "chargeOfEnginner" : "9001101",
     "eol" : false,
-    "subject" : "サブジェクト：",
+    "subject" : "SD-WANサービス年間保守契約（その１）",
     "amountOf" : 10000000,
-    "type" : "契約種別：製品保守",
-    "startDate" : "2015/04/01",
+    "type" : "クラウドサービス：SD-WANサービス",
+    "startDate" : "2016/04/01",
     "emergencyCaller" : "緊急連絡先：",
     "typeCode" : 1001,
-    "companyId" : "会社ID",
+    "companyId" : "CPY9101",
     "chargeOfSales" : "9000101",
     "automaticRenewal" : true,
-    "enteringDate" : "2015/04/01",
+    "enteringDate" : "2017/03/31",
     "masterContract" : "BC9101-00000-000001",
-    "endUser" : "エンドユーザー情報：",
+    "endUser" : "土屋様",
     "contract" : "DISC9101-0000-00001",
     "customerContractNo" : "顧客契約番号：",
     "orderBy" : "発注元：",
-    "responsibleParty" : "顧客担当：",
-    "caller" : "コール元会社名",
+    "responsibleParty" : "顧客担当：太郎くん",
+    "caller" : "コール元会社名：なし",
     "endDate" : "2017/03/31",
-    "fixedDate" : "2015/03/01",
+    "fixedDate" : "2016/03/25",
     "contractId" : "CONT00009101",
-    "typeOfConstraction" : "構築区分：",
-    "status" : "契約中"
+    "typeOfConstraction" : "構築区分：なし",
+    "status" : "契約締結済み"
 };
 
 var contracts_data = {
